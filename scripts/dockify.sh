a4a6e1134ed8632000070cce4e0c3b23511c5830
Matthew Hartley <Matthew.Hartley@jic.ac.uk>
2016-04-22 10:30:37 +0100

    Removed docker specific files

--- scripts/dockify.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DATADIR=`pwd`/data
-CODEDIR=`pwd`/scripts
-OUTPUTDIR=`pwd`/output
-CONTAINER=yeastprocessor
-
-#docker run -v $DATADIR:/data:ro -v $CODEDIR:/code:ro -v $OUTPUTDIR:/output -it --rm $CONTAINER python /code/ready.py
-docker run -d --link some-redis:redis -v $DATADIR:/data:ro -v $CODEDIR:/code:ro -v $OUTPUTDIR:/output $CONTAINER python /code/ready.py
--- scripts/popit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DATADIR=`pwd`/data
-CODEDIR=`pwd`/scripts
-OUTPUTDIR=`pwd`/output
-CONTAINER=yeastprocessor
-
-#docker run -v $DATADIR:/data:ro -v $CODEDIR:/code:ro -v $OUTPUTDIR:/output -it --rm $CONTAINER python /code/ready.py
-docker run --link some-redis:redis -v $DATADIR:/data:ro -v $CODEDIR:/code:ro -v $OUTPUTDIR:/output --rm $CONTAINER python /code/populate.py
